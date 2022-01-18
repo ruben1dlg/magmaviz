063993c9d381233ed53739c16543beb7a6b4c4cf
Mukund Iyer <msiyer@student.ubc.ca>
2022-01-17 22:54:01 -0800

    feat: adds title extraction from column names

--- src/magmaviz/boxplot.py
+++ src/magmaviz/boxplot.py
@@ -1,3 +1,7 @@
+import altair as alt
+import re
+
+
 def boxplot(df, x, y, facet=False):
     """Plot a boxplot with the magma color scheme and an option to facet.
 
@@ -8,7 +12,7 @@ def boxplot(df, x, y, facet=False):
     x : string
         Column name of the numerical variable to view the distribution of
     y : list
-        Column names of the categorical variables to assign boxes to
+        Column name containing the categorical variables to assign boxes to
     facet : boolean
         Determines whether separate graphs will be created for each category
 
@@ -22,18 +26,24 @@ def boxplot(df, x, y, facet=False):
     >>> from magmaviz.magmaviz import boxplot
     >>> boxplot(cars, "Miles_per_Gallon", "Origin", facet=True)
     """
-    # crete the plot object
+    # creating titles
+    x_title = re.sub(r"[_.,-]", " ", x)
+
+    y_title = re.sub(r"[_.,-]", " ", y)
+
+    # creating plot object
     plot = (
         alt.Chart(df)
         .mark_boxplot()
         .encode(
-            x=alt.X(x, title=x),
-            y=alt.Y(y, title=y),
+            x=alt.X(x, title=x_title),
+            y=alt.Y(y, title=y_title),
             color=alt.Color(y, scale=alt.Scale(scheme="magma")),
         )
     )
 
-    # facet if needed
+    # facet if required
+
     if facet == True:
         return plot.facet(row=y)
     else:
