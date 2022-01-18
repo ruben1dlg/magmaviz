39affd2d6f774d519d2d505a0003cb9eb5ab649a
Mukund Iyer <msiyer@student.ubc.ca>
2022-01-17 23:55:37 -0800

    test: adds output type  tests for boxplot

--- tests/test_boxplot.py
+++ tests/test_boxplot.py
@@ -26,3 +26,10 @@ def test_boxplot():
         raise TypeError(
             "'df' should be of type 'pandas.core.frame.DataFrame', a pandas dataframe."
         )
+
+    #Output tests
+    #checking if facetting is occurring correctly using type
+    assert type(boxplot(df, 'Miles_per_Gallon', 'Country_of_Origin', facet=True))==alt.vegalite.v4.api.FacetChart, "The output should be a faceted chart type"
+
+    assert type(boxplot(df, 'Miles_per_Gallon', 'Country_of_Origin, facet=False))==alt.vegalite.v4.api.Chart, "The output should be an altair chart type"
+
