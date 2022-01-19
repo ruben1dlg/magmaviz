781357e030bdb4d79e756b4f4697a438fc30a38d
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-01-18 22:34:45 -0800

    feat: Add opacity and size optional input parameters

--- src/magmaviz/scatterplot.py
+++ src/magmaviz/scatterplot.py
@@ -1,26 +1,32 @@
 import altair as alt
 import pandas as pd
+import re
 from pandas.api.types import is_string_dtype
 from pandas.api.types import is_numeric_dtype
-import re
 
-def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=False, yzero=False, shapes=True):
+def scatterplot(df, x, y, c="", t="", o=0.5, s=50, xtitle="", ytitle="", ctitle="", xzero=False, yzero=False, shapes=True):
     """Plot a scatterplot on the dataframe with the magma color scheme.
     
     Parameters
     ----------
     df : dataframe
-        Dataframe containing the numerical features x and y
+         Dataframe containing the numerical features x and y
     x : string
         Column-name of the numerical variable to be plotted on the x-axis
     y : string
         Column-name of the numerical variable to be plotted on the y-axis
     c : string
         Column-name of the categorical variable to color-code the data points
-        Default value is blank
+        Default value is blank for cases when there is no categorical column
     t : string
         Title of the plot. Default value is blank. If not provided,
         title will be computed based on x, y and/or c
+    o : float
+        Opacity of the data points
+        Default value is 0.5
+    s : integer
+        Size of the data points
+        Default value is 50
     xtitle : string
              Title of the x-axis. Default value is blank. If not provided,
              title will be proper case of the x axis column
@@ -51,6 +57,7 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
     >>> scatterplot(iris, "Sepal.Length", "Sepal.Width", Species)
     """
 
+    # Basic checks to see if parameters passed to function call contain expected values
     # check if the dataframe is a pandas dataframe
     if not isinstance(df, pd.core.frame.DataFrame):
         raise TypeError("'df' should be of type 'pandas.core.frame.DataFrame', a pandas dataframe.")
@@ -70,6 +77,14 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
     # check if title is a string
     if not isinstance(t, str):
         raise TypeError("Invalid value passed to 't' variable: Assign title as a 'string'.")
+
+    # check if opacity is a number
+    if not isinstance(o, float):
+        raise TypeError("Invalid value passed to 'o' variable: Assign opacity value as a decimal between 0 and 1.")
+
+    # check if size is an integer
+    if not isinstance(s, int):
+        raise TypeError("Invalid value passed to 's' variable: Assign size value as an integer between 1 and 100.")
 
     # check if x-axis title is a string
     if not isinstance(xtitle, str):
@@ -95,6 +110,7 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
     if not isinstance(shapes, bool):
         raise TypeError("Invalid value passed to 'shapes' variable: Assign boolean True to show different shapes for each color category.")
 
+    # Advanced checks to see whether columns exists, opacity and size are within expected range, etc.
     # check if column name assigned to x-axis is present in the dataframe
     assert x in list(
         df.columns
@@ -111,6 +127,14 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
             df.columns
         ), "The column specified for 'color' does not exist in the dataframe."
 
+    # check if opacity value is in the range 0.1 to 1.0
+    if o <= 0.1 or o > 1.0:
+        raise TypeError("Opacity value must be in the range [0.1, 1.0]")
+
+    # check if size value is in the range 1 to 100
+    if s < 1 or s > 100:
+        raise TypeError("Size of data points must be in the range [1, 100]")
+
     # check if x-axis column is numeric or not
     assert is_numeric_dtype(df[x]), "The column assigned to 'x' axis is not of type numeric."
 
@@ -121,7 +145,7 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
     if c != "":
         assert is_string_dtype(df[c]), "The column assigned to 'color' is not of type string."
 
-    # Added proper titles to axes, legend and plot
+    # add proper titles to axes, legend and plot
     if xtitle == "":
         xtitle = re.sub(r"[_.,-]", " ", x)
     if ytitle == "":
@@ -134,11 +158,12 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
         else:
             t = f"{xtitle.title()} vs {ytitle.title()} by {ctitle.title()}"
 
+    # scatterplot
     if c == "":
         plot = alt.Chart(
             data=df, title=t
         ).mark_point(
-            color="purple"
+            opacity=o, size=s, color="purple"
         ).encode(
             alt.X(x, title=xtitle.capitalize(), scale=alt.Scale(zero=xzero)),
             alt.Y(y, title=ytitle.capitalize(), scale=alt.Scale(zero=yzero))
@@ -148,6 +173,7 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
             plot = alt.Chart(
                 data=df, title=t
             ).mark_point(
+                opacity=o, size=s
             ).encode(
                 alt.X(x, title=xtitle.capitalize(), scale=alt.Scale(zero=xzero)),
                 alt.Y(y, title=ytitle.capitalize(), scale=alt.Scale(zero=yzero)),
@@ -157,6 +183,7 @@ def scatterplot(df, x, y, c="", t="", xtitle="", ytitle="", ctitle="", xzero=Fal
             plot = alt.Chart(
                 data=df, title=t
             ).mark_point(
+                opacity=o, size=s
             ).encode(
                 alt.X(x, title=xtitle.capitalize(), scale=alt.Scale(zero=xzero)),
                 alt.Y(y, title=ytitle.capitalize(), scale=alt.Scale(zero=yzero)),
