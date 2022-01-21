2611273129f9c69798a175fcb534f71c83a8d112
ruben1dlg <ruben1dlg@gmail.com>
2022-01-20 17:00:14 -0800

    Add corrplot function and tests. Added poetry.lock file

--- src/magmaviz/corrplot.py
+++ src/magmaviz/corrplot.py
@@ -1,3 +1,6 @@
+import altair as alt
+import pandas as pd
+
 def corrplot(df, print_corr=True, shape="circle"):
     """Generates a correlation plot for the numeric variables
     in a dataframe, and prints the correlation values.
@@ -25,3 +28,69 @@ def corrplot(df, print_corr=True, shape="circle"):
     >>> from magmaviz.magmaviz import corrplot
     >>> corrplot(movies_data, print_corr=True, shape="square")
     """
+
+    # Checking that the dataframe is a pandas dataframe type
+    if not isinstance(df, pd.core.frame.DataFrame):
+        raise TypeError(
+            "'df' should be of type 'pandas.core.frame.DataFrame', a pandas dataframe."
+        )
+    
+    # Checking that type for print_corr is a boolean
+    if not isinstance(print_corr, bool):
+        raise TypeError("'print_corr' should be of type 'boolean'.")
+        
+    # Checking that type of shape is string
+    if not isinstance(shape, str):
+        raise TypeError("'shape' should be of type 'str'.")
+
+    # Checking if shape has a valid value
+    if shape not in ["circle", "square"]:
+        raise ValueError("The value specified for 'shape' is not valid. It should be 'circle' or 'square'")
+
+    # Create the correlation dataframe
+    corr_df = (
+        df
+        .select_dtypes('number')
+        .corr('spearman')
+        .stack()                            # Get df into long format for altair
+        .reset_index(name='Correlation'))   # Name the index that is reset to "Correlation"
+
+    # Set cases where the variable is compared vs itself to zero
+    corr_df.loc[corr_df['Correlation'] == 1, 'Correlation'] = 0
+
+    # Add a column with the absolute value
+    corr_df['abs'] = corr_df['Correlation'].abs()
+
+    # Rename the columns
+    corr_df.rename(columns={'level_0': 'Variable1', 'level_1': 'Variable2'}, inplace=True)
+    
+    # Creating the plot with circles
+    if shape == "circle":
+        chart = alt.Chart(corr_df).mark_circle().encode(
+                    x='Variable1',
+                    y='Variable2',
+                    size='abs',
+                    color=alt.Color('Correlation', scale=alt.Scale(scheme='purpleorange', domain=(-1, 1))))
+    
+    # Creating the plot with squares
+    if shape == "square":
+        chart = alt.Chart(corr_df).mark_square().encode(
+                    x='Variable1',
+                    y='Variable2',
+                    size='abs',
+                    color=alt.Color('Correlation', scale=alt.Scale(scheme='purpleorange', domain=(-1, 1))))
+    
+    # Printing the correlations without repeating the combination of variables
+    if print_corr:
+        print_corr = corr_df.copy()
+        print_corr['Variable12'] = print_corr['Variable1'] + print_corr['Variable2']
+        print_corr['Variable21'] = print_corr['Variable2'] + print_corr['Variable1']
+
+        for value in print_corr['Variable21']:
+            if value in list(print_corr['Variable12']):
+                print_corr.drop(print_corr[print_corr.Variable21 == value].index, inplace=True)
+
+        print_corr.reset_index(drop=True, inplace=True)
+        print(print_corr[['Variable1', 'Variable2', 'Correlation']])
+    
+    return chart
---
+++ tests/test_corrplot.py
@@ -0,0 +1,80 @@
+from magmaviz.corrplot import corrplot
+import altair as alt
+import pandas as pd
+import pytest
+from vega_datasets import data
+
+
+def test_corrplot_error_for_df():
+    """Tests that an error is raised when df is not a dataframe"""
+    with pytest.raises(TypeError):
+        corrplot(["a", 2])
+
+def test_corrplot_error_for_print_corr():
+    """Tests that an error is raised when print_corr is not boolean"""
+    df = data.cars()
+    with pytest.raises(TypeError):
+        corrplot(df, 1)
+
+def test_corrplot_error_for_shape():
+    """Tests that an error is raised when shape is not string"""
+    df = data.cars()
+    with pytest.raises(TypeError):
+        corrplot(df, True, 2)
+
+def test_corrplot_invalid_shape():
+    """Tests an error is raised when shape has an invalid value"""
+    df = data.cars()
+    with pytest.raises(ValueError):
+        corrplot(df, True, "sphere")
+
+def test_corrplot_chart_shape_circle():
+    """Tests that the shape in the plot corresponds to the input parameters"""
+    df = data.cars()
+    plot = corrplot(df)
+    assert plot.mark == "circle", "The mark type of the chart should be 'circle'"
+
+def test_corrplot_chart_shape_square():
+    """Tests that the shape in the plot corresponds to the input parameters"""
+    df = data.cars()
+    plot = corrplot(df, shape="square")
+    assert plot.mark == "square", "The mark type of the chart should be 'square'"
+
+def test_corrplot_variable_vs_itself():
+    """Tests if the correlations between a variable vs itself are set to zero"""
+    df = data.cars()
+    plot = corrplot(df)
+    mysum = 0
+    for i in range(len(plot.data["Variable1"].tolist())):
+        if plot.data["Variable1"].tolist()[i] == plot.data["Variable2"].tolist()[i]:
+            mysum += plot.data["abs"].tolist()[i]
+
+    assert mysum == 0, "Correlations between the variable and itself are different to zero"
+
+def test_corrplot_number_of_numeric_variables():
+    """Tests if the plot is taking all the numeric variables in the dataframe"""
+    df = data.cars()
+    plot = corrplot(df)
+    assert (
+        len(df.select_dtypes('number').columns.tolist()) == len(plot.data["Variable1"].unique())
+    ), "The plot is not taking in consideration the complete set of numeric variables"
+
+def test_corrplot_output_chart_type():
+    """Tests that the output is of type Altair Chart"""
+    df = data.cars()
+    plot = corrplot(df)
+    assert (
+        type(plot) == alt.vegalite.v4.api.Chart
+    ), "The output should be an altair chart type"
+
+def test_corrplot_chart_columns_are_numeric():
+    """Tests if all the columns in the chart are numeric"""
+    df = data.cars()
+    plot = corrplot(df)
+    numeric_cols = plot.data["Variable1"].unique().tolist()
+    mysum = 0
+    for col in numeric_cols:
+        if df[col].dtype == 'float' or df[col].dtype == 'int':
+            mysum += 1
+
+    assert len(numeric_cols) == mysum, "There are columns used in the chart that are not numeric"  
