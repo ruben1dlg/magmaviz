bb01eff275f1107a995f01319b3c05b4a37d46ef
irene93 <irene93@student.ubc.ca>
2022-01-22 13:15:04 -0800

    Edit tests

--- tests/test_histogram.py
+++ tests/test_histogram.py
@@ -5,12 +5,6 @@ import pytest
 from vega_datasets import data
 
 
-# def test_histogram_error_for_df():
-#     """Tests that an error is raised when df is not a dataframe"""
-#     with pytest.raises(TypeError):
-#         histogram(["a", 'x', 'y'])
-
-
 def test_histogram():
     """Unit tests for the histogram function"""
     example1 = histogram(data.iris(), "species", "median(sepalLength)")
@@ -18,3 +12,35 @@ def test_histogram():
     assert example1.encoding.y.shorthand == 'median(sepalLength)', 'y_axis should be mapped to the y axis'
     assert example1.encoding.color.scale.scheme == 'magma', 'The color schema should be magma'
 
+
+def test_error_inputs_df():
+    """Check if TypeError is raised for incorrect input df"""
+    with pytest.raises(TypeError):
+        histogram('df','x','count()')
+        histogram([3,2,3,2], 'x', 'mean()')
+        histogram(1, 'x', 'mean()')
+        histogram(True, 'x', 'mean()')
+
+def test_error_inputs_x():
+    """Check if TypeError and ValueError are raised for incorrect input x"""
+    with pytest.raises(TypeError):
+        histogram(data.iris(), 1, 'mean()')
+        histogram(data.iris(), species, 'mean()')
+
+    with pytest.raises(ValueError):
+        histogram(data.iris(), 'brand', 'mean()')
+        histogram(data.iris(), 'specie', 'mean()')
+
+def test_validate():
+    """Unit tests for the validate function
+    
+        Check if ValueError and TypeError are raised for incorrect input y"""
+    with pytest.raises(TypeError):
+        histogram(data.iris(), 'species', True)
+        histogram(data.iris(), 'species', count())
+        histogram(data.iris(), 'species', 100.)
+
+    with pytest.raises(ValueError):
+        histogram(data.iris(), 'species', 'mean')
+        histogram(data.iris(), 'species', 'mean()')
+        histogram(data.iris(), 'species', 'mean(petal)')
