27bb8c18de602c1982c6e0cfda555d6074ce2fe8
Irene Yan <irene93@student.ubc.ca>
2022-01-22 18:18:34 +0000

    Update the docstring to specify all the supported aggregation operations

--- src/magmaviz/histogram.py
+++ src/magmaviz/histogram.py
@@ -1,5 +1,5 @@
 def histogram(df, x, y):
-    """Plot a histogram with the color scheme magma
+     """Plot a histogram with the magma color scheme
 
     Parameters
     ----------
@@ -8,7 +8,11 @@ def histogram(df, x, y):
     x : string
         Column name of the variable to be plotted on the x-axis
     y : string
-        An aggregation function to be plotted on the y-axis
+        An aggregation function to be plotted on the y-axis.
+        The supported aggregation operations are: ['average', 'count',
+        'distinct', 'max', 'mean', 'median', 'min', 'missing', 'product',
+        'q1', 'q3', 'ci0', 'ci1', 'stderr', 'stdev', 'stdevp', 'sum', 
+        'valid', 'values', 'variance', 'variancep']
 
     Returns
     -------
