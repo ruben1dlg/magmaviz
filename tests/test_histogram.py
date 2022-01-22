8f20397bd561e7d755f11473cb042b77935f9bb2
Irene Yan <irene93@student.ubc.ca>
2022-01-22 20:08:27 +0000

    Add unit test

---
+++ tests/test_histogram.py
@@ -0,0 +1,18 @@
+from magmaviz.histogram import histogram
+import altair as alt
+import pandas as pd
+import pytest
+from vega_datasets import data
+
+
+# def test_histogram_error_for_df():
+#     """Tests that an error is raised when df is not a dataframe"""
+#     with pytest.raises(TypeError):
+#         histogram(["a", 'x', 'y'])
+
+
+def test_histogram():
+    """Unit tests for the histogram function"""
+    example1 = histogram(data.iris(), "species", "median(sepalLength)")
+    assert example1.encoding.x.field == "species", 'x_axis should be mapped to the x axis'
+    assert example1.encoding.y.field == 'median(sepalLength)', 'y_axis should be mapped to the y axis'
