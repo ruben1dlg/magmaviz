be7ab3e459286d676349937475d61b590c614b52
Abdul Moid Mohammed <ibaad90@gmail.com>
2022-01-22 13:43:54 -0800

    feat: added new tests to check shapes = true branch

--- tests/test_scatterplot.py
+++ tests/test_scatterplot.py
@@ -17,8 +17,9 @@ sample_data = pd.DataFrame({
                                 10083831, 7988479, 9872424])
     })
 
-scatter = scatterplot(sample_data, "year", "no_of_cars", "brand")
-scatter
+scatter_default = scatterplot(df=sample_data, x="year", y="no_of_cars", c="brand",
+                      t="", o=0.5, s=50, xtitle="", ytitle="", ctitle="",
+                      xzero=False, yzero=False, shapes=False)
 
 
 def test_scatterplot():
@@ -30,40 +31,57 @@ def test_scatterplot():
 
 def test_enc_x_shorthand():
     """check if the x column is mapped to x-axis"""
-    assert scatter.encoding.x.shorthand == 'year', 'x should be mapped to the x-axis'
+    assert scatter_default.encoding.x.shorthand == 'year', 'x should be mapped to the x-axis'
 
 def test_enc_y_shorthand():
     """check if the y column is mapped to y-axis"""
-    assert scatter.encoding.y.shorthand == 'no_of_cars', 'y should be mapped to the y-axis'
+    assert scatter_default.encoding.y.shorthand == 'no_of_cars', 'y should be mapped to the y-axis'
 
 def test_color_title():
     """check if the color title reflects the color variable"""
-    assert scatter.encoding.color.title == "Brand"
+    assert scatter_default.encoding.color.title == "Brand"
 
 def test_enc_color_scheme():
     """check if the color scheme is set to magma"""
-    assert scatter.encoding.color.scale.scheme == 'magma', 'color scale scheme should be magma'
+    assert scatter_default.encoding.color.scale.scheme == 'magma', 'color scale scheme should be magma'
 
 def test_mark_type():
     """check if the mark type is point"""
-    assert scatter.mark.type == 'point', 'mark type should be a point'
+    assert scatter_default.mark.type == 'point', 'mark type should be a point'
 
 def test_mark_opacity():
     """check if the mark opacity is 0.5"""
-    assert scatter.mark.opacity == 0.5, 'mark opacity should be 0.5'
+    assert scatter_default.mark.opacity == 0.5, 'mark opacity should be 0.5'
 
 def test_mark_size():
     """check if the mark size is 50"""
-    assert scatter.mark.size == 50, 'mark size should be 50'
+    assert scatter_default.mark.size == 50, 'mark size should be 50'
 
 def test_enc_x_scale():
     """check if the x axis does not start from zero"""
-    assert scatter.encoding.x.scale.zero == False, 'x-axis should not start at 0'
+    assert scatter_default.encoding.x.scale.zero == False, 'x-axis should not start at 0'
 
 def test_enc_y_scale():
     """check if the y axis does not start from zero"""
-    assert scatter.encoding.y.scale.zero == False, 'y-axis should not start at 0'
+    assert scatter_default.encoding.y.scale.zero == False, 'y-axis should not start at 0'
 
 def test_plot_title():
     """check if the title is correctly updated based on field names"""
-    assert scatter.title == 'Year vs No Of Cars by Brand', 'title should be "Year vs No of Cars by Brand"'
+    assert scatter_default.title == 'Year vs No Of Cars by Brand', 'title should be "Year vs No of Cars by Brand"'
+
+
+scatter_new = scatterplot(df=sample_data, x="year", y="no_of_cars", c="brand",
+                          t="Scatterplot", o=0.5, s=40, xtitle="", ytitle="", ctitle="",
+                          xzero=False, yzero=False, shapes=True)
+
+def test_shapes():
+    """check if the shape is correctly assigned the categorical variable from c when shapes attribute is set to True"""
+    assert scatter_new.encoding.shape.shorthand == 'brand', 'the "brand" field must be assigned to shapes'
+
+def test_plot_title_custom():
+    """check if the title is correctly updated based on parameter"""
+    assert scatter_new.title == 'Scatterplot', 'title should be "Scatterplot"'
+
+def test_mark_size():
+    """check if the mark size is 40"""
+    assert scatter_new.mark.size == 40, 'mark size should be 40'
